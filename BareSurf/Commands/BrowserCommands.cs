d344c85df02b07ab0dd1d177db8e39b205098376
Nickleus Jimenez <Nickleus-J@live.com.ph>
2023-03-12 03:57:13 +0800

    Browser navigation command

---
+++ BareSurf/Commands/BrowserCommands.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BareSurf.Browser;
+using CefSharp.Wpf;
+
+namespace BareSurf.Commands
+{
+    public class BrowserCommands
+    {
+        public Run statusText { get; set; }
+        public ChromiumWebBrowser Browser { get; set; }
+        MainVM Model { get; set; }
+        public BrowserCommands(MainVM model, ChromiumWebBrowser _Browser, Run _statusText)
+        {
+            Model= model;
+            Browser= _Browser;
+            statusText= _statusText;
+        }
+
+        public void BrowseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            statusText.Text = "Executed";
+        }
+
+        public void BrowseCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            statusText.Text = "Can be done";
+        }
+
+        public void BrowsePageExecute()
+        {
+            if (!IsValidUrl(Model.SelectedBrowser.WebAddress))
+            {
+                Model.SelectedBrowser.WebAddress = "https://duckduckgo.com/?q=" + HttpUtility.UrlEncode(Model.SelectedBrowser.WebAddress);
+            }
+            Browser.Load(Model.SelectedBrowser.WebAddress);
+        }
+        public bool IsValidUrl(string url)
+        {
+            return !string.IsNullOrEmpty(url) && Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute) && url.IndexOf(".")>0 && url[url.Length-1]!='.';
+        }
+    }
+}
--- BareSurf/MainWindow.xaml.cs
+++ BareSurf/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BareSurf.Browser;
+using BareSurf.Commands;
 
 namespace BareSurf
 {
@@ -22,23 +23,26 @@ namespace BareSurf
     public partial class MainWindow : Window
     {
         protected MainVM Model { get; set; }
+        public BrowserCommands bCmd { get; set; }
         public MainWindow()
         {
             Model = new MainVM();
             Model.SelectedBrowser.WebAddress = "https://www.duckduckgo.com/";
             this.DataContext = Model;
+            
             InitializeComponent();
-
+            bCmd = new BrowserCommands(Model, Browser, executionText);
         }
+
         void LoadPageExecute(object target, ExecutedRoutedEventArgs e)
         {
-            Browser.Load(Model.SelectedBrowser.WebAddress);
+            bCmd.BrowsePageExecute();
         }
         private void OnKeyDownHandler(object sender, KeyEventArgs e)
         {
             if (e.Key != System.Windows.Input.Key.Enter && e.Key != System.Windows.Input.Key.Return && e.Key != System.Windows.Input.Key.BrowserRefresh) return;
-
-            Browser.Load(txtBoxAddress.Text);
+            Model.SelectedBrowser.WebAddress = txtBoxAddress.Text;
+            bCmd.BrowsePageExecute();
         }
         private void WebAddress_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
         {
