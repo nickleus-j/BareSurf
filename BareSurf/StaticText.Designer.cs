87cda2cdac1fc5721c5fe2ae38ecf1e453f03159
Nickleus Jimenez <Nickleus-J@live.com.ph>
2023-05-01 21:26:32 +0800

    try to push with policy in place

--- BareSurf/StaticText.Designer.cs
+++ BareSurf/StaticText.Designer.cs
@@ -88,7 +88,7 @@ namespace BareSurf {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://duckduckgo.com/?q=.
+        ///   Looks up a localized string similar to https://swisscows.com/en/web?query=.
         /// </summary>
         public static string DefaultSearchPage {
             get {
@@ -124,7 +124,7 @@ namespace BareSurf {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://duckduckgo.com/.
+        ///   Looks up a localized string similar to https://swisscows.com.
         /// </summary>
         public static string HomePage {
             get {
